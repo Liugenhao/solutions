137534cd1c5a7ad819db18a245ea630759b34995
Liugenhao <lgh.gg@qq.com>
2022-02-24 11:25:05 +0800

    del

--- solutions/src/layout/components/header/index.js
+++
@@ -1,7 +0,0 @@
-export default [
-  {title:"首页",url:"/"},
-  {title:"环境监控",url:"/contextMonitoring"},
-  {title:"视频监管",url:"/videoSurveillance"},
-  {title:"设备监管",url:"/deviceSupervise"},
-  {title:"人员监管",url:"/staffSupervise"},
-]
